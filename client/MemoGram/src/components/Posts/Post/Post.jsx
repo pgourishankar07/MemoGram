6dc160c87acab52144276879ba75149b873a95b6
pgourishankar07 <pgourishankar07@gmail.com>
2024-09-02 16:34:07 +0530

    posts rendering

--- client/MemoGram/src/components/Posts/Post/Post.jsx
+++ client/MemoGram/src/components/Posts/Post/Post.jsx
@@ -1,7 +1,69 @@
+/* eslint-disable react/prop-types */
 import useStyles from "./Post.js";
-const Post = () => {
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@mui/material";
+import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
+import DeleteIcon from "@mui/icons-material/Delete";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import moment from "moment";
+
+const Post = ({ post }) => {
   const classes = useStyles();
-  return <div>Post</div>;
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image={
+          post.selectedFile ||
+          "https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png"
+        }
+        title={post.title}
+      />
+      <div className={classes.overlay}>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">
+          {moment(post.createdAt).fromNow()}
+        </Typography>
+      </div>
+      <div className={classes.overlay2}>
+        <Button style={{ color: "white" }} size="small" onClick={() => {}}>
+          <MoreHorizIcon fontSize="default" />
+        </Button>
+      </div>
+      <div className={classes.details}>
+        <Typography variant="body2" color="textSecondary" component="h2">
+          {post.tags.map((tag) => `#${tag} `)}
+        </Typography>
+      </div>
+      <Typography
+        className={classes.title}
+        gutterBottom
+        variant="h5"
+        component="h2"
+      >
+        {post.title}
+      </Typography>
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {post.message}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary" onClick={() => {}}>
+          <ThumbUpAltIcon fontSize="small" /> Like {post.likeCount}{" "}
+        </Button>
+        <Button size="small" color="primary" onClick={() => {}}>
+          <DeleteIcon fontSize="small" /> Delete
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default Post;
--- client/MemoGram/src/components/Posts/Posts.jsx
+++ client/MemoGram/src/components/Posts/Posts.jsx
@@ -1,15 +1,29 @@
 import { useSelector } from "react-redux";
 import Post from "./Post/Post.jsx";
 import useStyles from "./Posts.js";
+
+import { CircularProgress, Grid } from "@mui/material";
+
 const Posts = () => {
   const classes = useStyles();
   const posts = useSelector((state) => state.posts);
   console.log(posts);
 
-  return (
-    <>
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6} md={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
