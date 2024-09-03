3eb3987c2f466d508e154b9fa3edde1ae291d548
pgourishankar07 <pgourishankar07@gmail.com>
2024-09-03 09:02:04 +0530

    completed linking

--- client/MemoGram/src/api/index.js
+++ client/MemoGram/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:9000" });
+const API = axios.create({ baseURL: "https://memogram-7dib.onrender.com" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
